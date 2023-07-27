ea6abd630555e1ac94a5645643196a6c721a7be5
EmayaServices <132591460+EmayaServices@users.noreply.github.com>
2023-07-27 09:07:03 -0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.sample-slider', {
+        loop: true,
+        speed: 2000,
+        slidesPerView: 3,      
+        autoplay: {
+            delay: 0,
+        },
+    })
